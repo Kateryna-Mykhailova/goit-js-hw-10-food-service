83ef444c3f75a7a55c26e5dc9165658d2be23ead
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-08-09 14:45:59 +0300

    cсоздает и рендорит разметку меню

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+import menuCardTpl from './templates/menu-card.hbs';
+import menu from './menu.json';
 import './sass/main.scss';
+console.log(menuCardTpl);
+
+// const menuMarkup = createMenuMarkup(menu);
+// function createMenuMarkup(menu){
+//     return menu.map(el => menuCardTpl(menu)).join('');
+// };
+// console.log(menuMarkup);
+const menuMarkup = createMenuMarkup(menu);
+
+function createMenuMarkup(menu){
+    return menu.map(menuCardTpl).join('');
+};
+console.log(menuMarkup);
+
+const menuParentEl = document.querySelector(".menu");
+console.log(menuParentEl);
+
+menuParentEl.insertAdjacentHTML("afterend", menuMarkup);
